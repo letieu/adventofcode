4321a0ccc6c841a02fac3d6bd05062dbf23dd977
letieu <letieu8@gmail.com>
2023-08-16 13:55:49 +0700

    don't need check min

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,11 @@ fn main() {
 
     for line in content.lines() {
         if line.is_empty() {
-            let min = top_three.iter().min().unwrap();
-
-            if &block_sum > min {
+            if block_sum > top_three[0] {
                 top_three[0] = block_sum;
                 top_three.sort();
             }
 
-            // reset block_sum
             block_sum = 0;
             continue;
         }
